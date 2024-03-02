02af89775d70a0aee8fb95b8b3cf93bc3ece3e55
Aziz Ramdan Kurniawan <azizramdan44@gmail.com>
2024-03-02 20:18:31 +0700

    feat: add dark mode toggle

--- assets/script.js
+++ assets/script.js
@@ -6,4 +6,14 @@ document.addEventListener('alpine:init', () => {
       this.on = !this.on
     }
   })
+
+  Alpine.store('darkMode', {
+    on: localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches),
+
+    toggle() {
+      this.on = !this.on
+
+      localStorage.theme = this.on ? 'dark' : 'light'
+    }
+  })
 })
